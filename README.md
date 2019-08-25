46d2c8a459bb035100fad0c21bf57523f575e0e2
TerrenceMM2 <terrencemm2@gmail.com>
2019-08-24 22:43:19 -0500

    renders results to handlebars page and styles cards

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # coursework14-news-scrape
 Vanderbilt Coding Boot Camp - Coursework 14 - News Scrape (MongoDB/Mongoose/Cheerio)
+
+#### Sources
+https://dev.to/bananabrann/express-routing-for-noobs-pretend-youre-a-computer-walkthrough-1ma4
--- controllers/articles.js
+++ controllers/articles.js
@@ -1,7 +1,36 @@
 var Article = require('../models/Article');
+var cheerio = require("cheerio");
+var axios = require("axios");
 
 module.exports = {
     get: (req, res) => {
-        res.render("index", { data: "You've searched for articles." });
+
+        axios.get("https://www.newyorker.com/").then(function (response) {
+
+            var $ = cheerio.load(response.data);
+            var results = [];
+
+            $(".Card__content___2_jDO").each(function (i, element) {
+
+                var title = $(element).find(".Card__hed___31cLY").text();
+                var summary = $(element).find(".Card__dek___29Iu1").text();
+                var articleUrl = $(element).find("a:nth-child(2)").attr("href");
+                var photoUrl = $(element).find("img").attr("src");
+
+                results.push({
+                    title,
+                    summary,
+                    articleUrl: "https://www.newyorker.com" + articleUrl,
+                    photoUrl
+                });
+            });
+
+            Article.create(results).then(function(data) {
+                res.status(200).render("results", {data: data, count: results.length});
+            }).catch(function(err) {
+                res.status(500).json(err);
+            });
+
+        });
     }
 };
