ab1087f37f690fb36dacec271615ebdead8c98fa
TerrenceMM2 <terrencemm2@gmail.com>
2019-08-29 16:24:59 -0500

    deletes article and renders content

--- routes/article.js
+++ routes/article.js
@@ -4,6 +4,11 @@ var cheerio = require("cheerio");
 var axios = require("axios");
 
 // var articleController = require("../controllers/articles");
+
+// router.get('/api/put', articleController.put);
+// router.get('/', articleController.get);
+// localhost:8080/article/api/put
+
 var Article = require('../models/Article');
 
 router.get('/', function (req, res) {
@@ -21,19 +26,19 @@ router.get('/', function (req, res) {
 
 router.get('/fetch', function (req, res) {
 
-    Article.remove({}).then(function(data){
+    Article.deleteMany({}).then(function (data) {
         axios.get("https://www.newyorker.com/").then(function (response) {
 
             var $ = cheerio.load(response.data);
             var results = [];
-    
+
             $(".Card__content___2_jDO").each(function (i, element) {
-    
+
                 var title = $(element).find(".Card__hed___31cLY").text();
                 var summary = $(element).find(".Card__dek___29Iu1").text();
                 var articleUrl = $(element).find("a:nth-child(2)").attr("href");
                 var photoUrl = $(element).find("img").attr("src");
-    
+
                 results.push({
                     title,
                     summary,
@@ -41,7 +46,7 @@ router.get('/fetch', function (req, res) {
                     photoUrl
                 });
             });
-    
+
             Article.create(results).then(function (data) {
                 res.locals.metaTags = {
                     title: "MongoLines | Articles"
@@ -53,32 +58,26 @@ router.get('/fetch', function (req, res) {
             }).catch(function (err) {
                 res.status(500).json(err);
             });
-    
-        }).catch(function(err){
+
+        }).catch(function (err) {
             res.status(500).json(err);
         })
     })
-    
+
 });
 
-router.delete('/delete/:id', function (req, res) {
-    Article.findByIdAndDelete(req.params.id).then(function (data) {
-        Article.find({}).then(function (data) {
-            res.render("results", {data}).status(200);
-        }).catch(function (err) {
-            res.json(err);
-            // res.status(500).render("404", {
-            //     err,
-            //     msg: "Lookup error."
-            // });
+router.delete("/delete", function (req, res) {
+    Article.deleteOne({"_id": req.body.data}).then( () => {
+            Article.find({});
         })
-    }).catch(function (err) {
-        res.json(err);
-        // res.status(500).render("404", {
-        //     err,
-        //     msg: "Deletion error."
-        // });
-    })
+        .then(function (data) {
+            res.render("results", {
+                data
+            });
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
 });
 
 module.exports = router;
